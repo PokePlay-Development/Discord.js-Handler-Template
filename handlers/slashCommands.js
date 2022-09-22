d461e8fbb0d0e61fef27961512e455f72e8836c2
TunerBotDev <92864809+TunerBotDev@users.noreply.github.com>
2022-09-22 12:55:14 +0530

    Update slashCommands.js

--- handlers/slashCommands.js
+++ handlers/slashCommands.js
@@ -1,163 +1,214 @@
-const { readdirSync, lstatSync } = require("fs");
-const { SlashCommandBuilder } = require("discord.js");
-const dirSetup = require("../settings.json").applicationCommandDirs;
-const { loadSlashGlobal } = require("../settings.json")
-module.exports = (client) => {
-    try {
-		let allCommands = [];
-        readdirSync("./slashCommands/").forEach((dir) => {
-			if(lstatSync(`./slashCommands/${dir}`).isDirectory()) {
-				const groupName = dir;
-				const cmdSetup = dirSetup.find(d=>d.Folder == dir);
-				//If its a valid cmdsetup
-				if(cmdSetup && cmdSetup.Folder) {
-					//Set the SubCommand as a Slash Builder
-					const subCommand = new SlashCommandBuilder().setName(String(cmdSetup.CmdName).replace(/\s+/g, '_').toLowerCase()).setDescription(String(cmdSetup.CmdDescription));
-					//Now for each file in that subcommand, add a command!
+const { SlashCommandBuilder, REST, Routes } = require("discord.js")
+const config = require("../settings.json")
+const folderConfig = config.slashCommandsDirs;
+const { readdirSync, lstatSync } = require("node:fs");
+module.exports = async (client) => {
+	try {
+		let allCommands = new Array();
+		readdirSync(`${process.cwd()}/slashCommands/`).forEach((dir) => {
+			if(lstatSync(`${process.cwd()}/slashCommands/${dir}`).isDirectory()) {
+				let cmdSetup = folderConfig.find(r => r.Folder == dir)
+				if(!cmdSetup) return;
+				if(cmdSetup) {
+					const subCommand = new SlashCommandBuilder().setName(cmdSetup.CmdName.america.replace(/\s+/g, "_")).setDescription(cmdSetup.CmdDescription);
 					const slashCommands = readdirSync(`./slashCommands/${dir}/`).filter((file) => file.endsWith(".js"));
-					for (let file of slashCommands) {
+					for(let file of slashCommands) {
 						let pull = require(`../slashCommands/${dir}/${file}`);
-						if (pull.name && pull.description) {
-							subCommand
-							.addSubcommand((subcommand) => {
-								subcommand.setName(String(pull.name).toLowerCase()).setDescription(pull.description)
-								if(pull.options && pull.options.length > 0){
-									for(const option of pull.options){
-										if(option.User && option.User.name && option.User.description){
-											subcommand.addUserOption((op) =>
-												op.setName(String(option.User.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.User.description).setRequired(option.User.required)
-											)
-										} else if(option.Integer && option.Integer.name && option.Integer.description){
-											subcommand.addIntegerOption((op) =>
-												op.setName(String(option.Integer.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Integer.description).setRequired(option.Integer.required)
-											)
-										} else if(option.String && option.String.name && option.String.description){
-											subcommand.addStringOption((op) =>
-												op.setName(String(option.String.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.String.description).setRequired(option.String.required)
-											)
-										} else if(option.Channel && option.Channel.name && option.Channel.description){
-											subcommand.addChannelOption((op) =>
-												op.setName(String(option.Channel.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Channel.description).setRequired(option.Channel.required)
-											)
-										} else if(option.Role && option.Role.name && option.Role.description){
-											subcommand.addRoleOption((op) =>
-												op.setName(String(option.Role.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Role.description).setRequired(option.Role.required)
-											)
-										} else if(option.StringChoices && option.StringChoices.name && option.StringChoices.description && option.StringChoices.choices && option.StringChoices.choices.length > 0){
-											subcommand.addStringOption((op) =>
-												op.setName(String(option.StringChoices.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.StringChoices.description).setRequired(option.StringChoices.required)
-												.addChoices(option.StringChoices.choices.map(c=> [String(c[0]).replace(/\s+/g, '_').toLowerCase(),String(c[1])] )),
-											)
-										} else if(option.IntChoices && option.IntChoices.name && option.IntChoices.description && option.IntChoices.choices && option.IntChoices.choices.length > 0){
-											subcommand.addStringOption((op) =>
-												op.setName(String(option.IntChoices.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.IntChoices.description).setRequired(option.IntChoices.required)
-												.addChoices(option.IntChoices.choices.map(c=> [String(c[0]).replace(/\s+/g, '_').toLowerCase(),parseInt(c[1])] )),
-											)
+						if(pull.name && pull.description) {
+							subCommand.addSubcommand((sub) => {
+								sub.setName(String(pull.name)).setDescription(pull.description)
+								if(pull.options && pull.options.length > 0) {
+									for(option of pull.options) {
+										if(option.type && option.name && option.description) {
+											let type = option.type;
+											let name = option.name.replace(/\s+/g, "_").toLowerCase();
+											let required = false;
+											if(option.required && option.required == true) {
+												required = true
+											}
+											if(type == "User") {
+												sub.addUserOption((op) => {
+													op.setName(name).setDescription(option.description).setRequired(required)
+												})
+											}
+											if(type == "Integer") {
+												sub.addIntegerOption((op) => {
+													op.setName(name).setDescription(option.description).setRequired(required)
+												})
+											}
+											if(type == "String") {
+												sub.addStringOption((op) => {
+													op.setName(name).setDescription(option.description).setRequired(required)
+												})
+											}
+											if(type == "Channel") {
+												sub.addChannelOption((op) => {
+													op.setName(name).setDescription(option.description).setRequired(required)
+												})
+											}
+											if(type == "Role") {
+												sub.addRoleOption((op) => {
+													op.setName(name).setDescription(option.description).setRequired(required)
+												})
+											}
+											if(type == "Choices") {
+												if(option.choices && option.choices.length > 0) {
+													sub.addStringOption((op) => {
+														op.setName(name).setDescription(option.description).setRequired(required)
+														option.choices.forEach(choice => {
+															op.addChoices(
+																{ name: `${choice.name}`, value: `${choice.description}`}
+															)
+														})
+													})
+												}
+											} else {
+												console.log(`[HANDLER]`.bold.red, "Uknown", "InteractionCommandOption".brightRed, "Provided.")
+											}
 										} else {
-											console.log(`A Option is missing the Name or/and the Description of ${pull.name}`)
+											console.log(`Unable To Add option.`)
 										}
 									}
 								}
-								return subcommand;
+								return subCommand;
 							})
 							client.slashCommands.set(String(cmdSetup.CmdName).replace(/\s+/g, '_').toLowerCase() + pull.name, pull)
 						} else {
-							console.log(file, `error -> missing a help.name, or help.name is not a string.`.brightRed);
+							console.log(file, `error -> missing a help.name, or help.name is not a string.`.brightGreen);
 							continue;
 						}
 					}
-					//add the subcommand to the array
-					allCommands.push(subCommand.toJSON());
-				} 
-				else {
-					return console.log(`The Subcommand-Folder ${dir} is not in the dirSetup Configuration!`);
+					allCommands.push(subCommand.toJSON())
+				} else {
+					console.log(`Unable TO Find Folder Configuration.`)
 				}
 			} else {
-				let pull = require(`../slashCommands/${dir}`);
-				if (pull.name && pull.description) {
-					let Command = new SlashCommandBuilder().setName(String(pull.name).toLowerCase()).setDescription(pull.description);
-						if(pull.options && pull.options.length > 0){
-							for(const option of pull.options){
-								if(option.User && option.User.name && option.User.description){
-									Command.addUserOption((op) =>
-										op.setName(String(option.User.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.User.description).setRequired(option.User.required)
-									)
-								} else if(option.Integer && option.Integer.name && option.Integer.description){
-									Command.addIntegerOption((op) =>
-										op.setName(String(option.Integer.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Integer.description).setRequired(option.Integer.required)
-									)
-								} else if(option.String && option.String.name && option.String.description){
-									Command.addStringOption((op) =>
-										op.setName(String(option.String.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.String.description).setRequired(option.String.required)
-									)
-								} else if(option.Channel && option.Channel.name && option.Channel.description){
-									Command.addChannelOption((op) =>
-										op.setName(String(option.Channel.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Channel.description).setRequired(option.Channel.required)
-									)
-								} else if(option.Role && option.Role.name && option.Role.description){
-									Command.addRoleOption((op) =>
-										op.setName(String(option.Role.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Role.description).setRequired(option.Role.required)
-									)
-								} else if(option.StringChoices && option.StringChoices.name && option.StringChoices.description && option.StringChoices.choices && option.StringChoices.choices.length > 0){
-									Command.addStringOption((op) =>
-										op.setName(String(option.StringChoices.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.StringChoices.description).setRequired(option.StringChoices.required)
-										.addChoices(option.StringChoices.choices.map(c=> [String(c[0]).replace(/\s+/g, '_').toLowerCase(),String(c[1])] )),
-									)
-								} else if(option.IntChoices && option.IntChoices.name && option.IntChoices.description && option.IntChoices.choices && option.IntChoices.choices.length > 0){
-									Command.addStringOption((op) =>
-										op.setName(String(option.IntChoices.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.IntChoices.description).setRequired(option.IntChoices.required)
-										.addChoices(option.IntChoices.choices.map(c=> [String(c[0]).replace(/\s+/g, '_').toLowerCase(),parseInt(c[1])] )),
-									)
-								} else {
-									console.log(`A Option is missing the Name or/and the Description of ${pull.name}`)
+				let pull = require(`../slashCommands/${dir}`)
+				if(pull.name && pull.description) {
+					let sub = new SlashCommandBuilder().setName(String(pull.name).toLowerCase()).setDescription(String(pull.description))
+					if(pull.options && pull.options.length > 0) {
+						for(option of pull.options) {
+							if(option.type && option.name && option.description) {
+								let type = option.type;
+								let name = option.name.replace(/\s+/g, "_").toLowerCase();
+								let required = false;
+								if(option.required && option.required == true) {
+									required = true
 								}
+								if(type == "User") {
+									sub.addUserOption((op) => {
+										op.setName(name).setDescription(option.description).setRequired(required)
+									})
+								}
+								if(type == "Integer") {
+									sub.addIntegerOption((op) => {
+										op.setName(name).setDescription(option.description).setRequired(required)
+									})
+								}
+								if(type == "String") {
+									sub.addStringOption((op) => {
+										op.setName(name).setDescription(option.description).setRequired(required)
+									})
+								}
+								if(type == "Channel") {
+									sub.addChannelOption((op) => {
+										op.setName(name).setDescription(option.description).setRequired(required)
+									})
+								}
+								if(type == "Role") {
+									sub.addRoleOption((op) => {
+										op.setName(name).setDescription(option.description).setRequired(required)
+									})
+								}
+								if(type == "Choices") {
+									if(option.choices && option.choices.length > 0) {
+										sub.addStringOption((op) => {
+											op.setName(name).setDescription(option.description).setRequired(required)
+											option.choices.forEach(choice => {
+												op.addChoices(
+													{ name: `${choice.name}`, value: `${choice.description}`}
+												)
+											})
+										})
+									}
+								} else {
+									console.log(`[HANDLER]`.bold.red, "Uknown", "InteractionCommandOption".brightRed, "Provided.")
+								}
+							} else {
+								console.log(`Unable To Add option.`)
 							}
 						}
-						allCommands.push(Command.toJSON());
-						client.slashCommands.set("normal" + pull.name, pull)
-				} 
-				else {
-					console.log(file, `error -> missing a help.name, or help.name is not a string.`.brightRed);
-				}
-			}
-        });
-        
-		//Once the Bot is ready, add all Slas Commands to each guild
-		client.on("ready", () => {
-			if(loadSlashGlobal){
-				client.application.commands.set(allCommands)
-				.then(slashCommandsData => {
-					console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.red} Loaded for all: ${`All possible Guilds`}`.brightRed); 
-					console.log(`Because u are Using Global Settings, it can take up to 1 hour until the Commands are changed!`.bold.green)
-				}).catch((e)=>console.log(e));
-			} else {
-				client.guilds.cache.map(g => g).forEach((guild) => {
-					try{
-						guild.commands.set(allCommands)
-						.then(slashCommandsData => {
-							console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.red} Loaded for: ${`${guild.name}`}`.brightRed); 
-						}).catch((e)=>console.log(e));
-					}catch (e){
-						console.log(String(e).grey)
 					}
-				});
-			}
-		})
-		//DISABLE WHEN USING GLOBAL!
-		client.on("guildCreate", (guild) => {
-			try{
-				if(!loadSlashGlobal){
-					guild.commands.set(allCommands)
-						.then(slashCommandsData => {
-							console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.green} Loaded for: ${`${guild.name}`.underline}`.brightGreen); 
-						}).catch((e)=>console.log(e));
+					allCommands.push(sub.toJSON())
+					client.slashCommands.set("normal" + pull.name, pull)
+				} else {
+					console.log(file, `error -> missing a help.name, or help.name is not a string.`.brightGreen);
 				}
-			}catch (e){
-				console.log(String(e).grey)
+			}
+		});
+		// LOADING ALL COMMANDS INTO THE BOT.
+		client.on("ready", async () => {
+			if(config.loadSlashsGlobal == false) {
+				const token = client.token;
+				const rest = new REST({ version: 10 }).setToken(token);
+				rest.put(Routes.applicationCommands(client.user.id), { body: [] })
+				client.guilds.cache.forEach((guild) => {
+					let guildId = guild.id;
+					(async () => {
+						try {
+							const data = await rest.put(
+								Routes.applicationGuildCommands(client.user.id, guildId),
+								{ body: allCommands },
+							);
+							console.log(`${`Successfully`.bold.green} Loaded ${`(/)`.brightGreen} ${allCommands.length} Commands For: ${`${guild.name}`.brightRed}`);
+						} catch (error) {
+							console.log(`${`Unable`.bold.red} To Load ${`(/)`.brightGreen} ${allCommands.length} Commands For: ${`${guild.name}`.brightRed}`);
+						}
+					})();
+				})
+			}
+			if(config.loadSlashsGlobal == true) {
+				const token = client.token;
+				const rest = new REST({ version: 10 }).setToken(token);
+				client.guilds.cache.forEach((guild) => {
+					let guildId = guild.id;
+					rest.put(Routes.applicationGuildCommands(client.user.id, guildId), { body: [] })
+				})
+				try {
+					const data = await rest.put(
+						Routes.applicationCommands(client.user.id),
+						{ body: allCommands },
+					);
+					console.log(`${`Successfully`.bold.green} Loaded ${`(/)`.brightGreen} ${allCommands.length} Commands For: ${`ALL POSSIBLE GUILDS`.brightRed}`);
+				} catch (error) {
+					console.log(`${`Unable`.bold.red} To Load ${`(/)`.brightGreen} ${allCommands.length} Commands For: ${`ALL POSSIBLE GUILDS`.brightRed}`);
+				}
 			}
 		})
-		
-    } catch (e) {
-        console.log(String(e.stack).bgRed)
-    }
-};
+		client.on("guildCreate", async (guild) => {
+			try {
+				if(!config.loadSlashsGlobal) {
+					let guildId = guild.id;
+					const token = client.token;
+					const rest = new REST({ version: '10' }).setToken(token);
+					(async () => {
+						try {
+							const data = await rest.put(
+								Routes.applicationGuildCommands(client.user.id, guildId),
+								{ body: allCommands },
+							);
+					
+							console.log(`${`Successfully`.bold.green} Loaded ${`(/)`.brightGreen} ${allCommands.length} Commands For: ${`${guild.name}`.brightRed}`);
+						} catch (error) {
+							console.log(`${`Unable`.bold.red} To Load ${`(/)`.brightGreen} ${allCommands.length} Commands For: ${`${guild.name}`.brightRed}`);
+						}
+					})();
+				}
+			} catch (e) {
+				console.log(String(e.stack).bgRed)
+			}
+		})
+	} catch (e) {
+		console.log(String(e.stack).bgRed)
+	}
+}
