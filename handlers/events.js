9462a3d7ff8128ab559c8ecfc1c3c28f53be927c
Krish Srivastava <krishsrivastava2006@gmail.com>
2023-09-25 12:35:24 +0530

    RestEvents Bug Fixed

--- handlers/events.js
+++ handlers/events.js
@@ -6,18 +6,21 @@ module.exports = async (client) => {
       // The Client And Guild Folder is Just To Help Organise Event Files.
      // Load All The RestEvents.
      readdirSync(`${process.cwd()}/events/RestEvents/`).forEach(async file => {
+	if (!file.includes(".js")) return;
         let fileName = file.replace(".js", "")
         client.rest.on(fileName, require(`../events/RestEvents/${file}`).bind(null, client));
         eventCount = eventCount + 1
     })
     // load all events in the Folder Named Guild
     readdirSync(`${process.cwd()}/events/Guild/`).forEach(async file => {
+	if (!file.includes(".js")) return;
         let fileName = file.replace(".js", "")
         client.on(fileName, require(`../events/Guild/${file}`).bind(null, client));
         eventCount = eventCount + 1
     })
     // Load All The Events in The Folder Named Client
     readdirSync(`${process.cwd()}/events/Client/`).forEach(async file => {
+	if (!file.includes(".js")) return;
         let fileName = file.replace(".js", "")
         client.on(fileName, require(`../events/Client/${file}`).bind(null, client));
         eventCount = eventCount + 1;
